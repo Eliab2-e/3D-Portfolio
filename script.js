105099dae46aaf1ad5567b23d7b20297a0e2d390
Eliab2-e <elijahab8@gmail.com>
2025-08-19 19:40:40 -0400

    LFS

---
+++ script.js
@@ -0,0 +1,63 @@
+// Add click event listeners for #slide-2s (mobile) and #slide-2 (desktop)
+
+document.getElementById('slide-1s').addEventListener('click', function() {
+    window.open("Trainstation.html", '_blank');
+});
+
+document.getElementById('slide-1').addEventListener('click', function() {
+    window.open("Trainstation.html", '_blank');
+});
+
+
+document.getElementById('slide-2s').addEventListener('click', function() {
+    window.open("Workspace.html", '_blank');
+});
+
+document.getElementById('slide-2').addEventListener('click', function() {
+    window.open("Workspace.html", '_blank');
+});
+
+
+document.getElementById('slide-3s').addEventListener('click', function() {
+    window.open("House.html", '_blank');
+});
+
+document.getElementById('slide-3').addEventListener('click', function() {
+    window.open("House.html", '_blank');
+});
+
+
+document.getElementById('slide-4s').addEventListener('click', function() {
+    window.open("Lamp.html", '_blank');
+});
+
+document.getElementById('slide-4').addEventListener('click', function() {
+    window.open("Lamp.html", '_blank');
+});
+
+
+document.getElementById('slide-5s').addEventListener('click', function() {
+    window.open("Bottle.html", '_blank');
+});
+
+document.getElementById('slide-5').addEventListener('click', function() {
+    window.open("Bottle.html", '_blank');
+});
+
+
+document.getElementById('slide-6s').addEventListener('click', function() {
+    window.open("Anvil.html", '_blank');
+});
+
+document.getElementById('slide-6').addEventListener('click', function() {
+    window.open("Anvil.html", '_blank');
+});
+
+
+document.getElementById('slide-7s').addEventListener('click', function() {
+    window.open("Donut.html", '_blank');
+});
+
+document.getElementById('slide-7').addEventListener('click', function() {
+    window.open("Donut.html", '_blank');
+});
